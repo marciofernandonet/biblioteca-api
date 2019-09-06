c8a3cb129a4f5b9373065feda9f29d0879d80db1
marcio <marciofernandonet@gmail.com>
2019-09-06 20:02:33 -0300

    url_con

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose
-    //.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, { useNewUrlParser:true, useFindAndModify: false })
-    
-    .connect('mongodb+srv://userb:userb123@clustermongo-l9enx.mongodb.net/biblioteca', { useNewUrlParser: true, useFindAndModify: false })
-    
+    .connect(process.env.URL_CONNECT, { useNewUrlParser:true, useFindAndModify: false })  
     .then(() => { 
         console.log('Database Connected');
     }).catch(err => {
         console.error(`Error: ${err}`);
         throw err;
-    });
+});
